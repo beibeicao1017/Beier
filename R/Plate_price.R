b03b67ee605ef7eff378a9a179a9bcd5f671db2f
beiercao1017 <beiercao1017@ucla.edu>
2020-12-13 18:05:38 +0800

    -

--- R/Plate_price.R
+++
@@ -1,8 +0,0 @@
-#' Shanghai license plate deal price
-#'
-#' @format 166 x 3 dataframe
-#'
-#' @examples
-"Plate_price"
-Plate_price = read.csv("Shanghai License Plate deal price data.csv")
-usethis::use_data(Plate_price,overwrite = TRUE)
