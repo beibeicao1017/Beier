a61e2e6c767d4ef33902b47b9b1f4cebfc918a41
beiercao1017 <beiercao1017@ucla.edu>
2020-12-13 17:22:32 +0800

    1

---
+++ R/adding.R
@@ -0,0 +1,13 @@
+#' Adding
+#'
+#' @param a  any interge
+#' @param b any interge
+#'
+#' @return any interge
+#' @export
+#'
+#' @examples
+#' adding(1,2)
+adding <- function(a,b){
+  return(a+b)
+}
