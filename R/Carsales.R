ece853a7dd6066fd1b61fe904cac198f45374545
beiercao1017 <beiercao1017@ucla.edu>
2020-12-13 18:55:18 +0800

    ~~~

---
+++ R/Carsales.R
@@ -0,0 +1,13 @@
+#' Top Cars Sales
+#'
+#' @format 3148 x 12 dataframe
+#'
+#' \describe{
+#' \item{index}{id}
+#' \item{column1}{salesperson}
+#' \item{column2}{city}
+#' }
+#'
+"Carsales"
+Carsales = read.csv("Top Cars Sales.csv")
+usethis::use_data(Carsales, overwrite = TRUE)
